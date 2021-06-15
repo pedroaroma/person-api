d16069c964a130087e2eddbcebe1c4381aa5cd84
Pedro Luiz Aroma Filho <pedroaroma@pop-os.localdomain>
2021-06-15 20:52:09 -0300

    Added list all persons endpoint

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,10 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
@@ -28,4 +32,15 @@ public class PersonService {
                 .build();
     }
 
+    public List<PersonDTO> getAllPersons() {
+
+        List<Person> personList = personRepository.findAll();
+//        List<PersonDTO> personDTOList = new ArrayList<>();
+//        personList.forEach(person -> personDTOList.add(personMapper.toDTO(person)));
+//
+//        return personDTOList;
+        return personList.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
